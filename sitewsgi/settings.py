8c19a3247fc96c3d0f3849490e961897b0431236
lalchan123 <lalchanbadsa12345@gmail.com>
2024-10-19 12:10:49 -0600

    lalchan

--- sitewsgi/settings.py
+++ sitewsgi/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(^97d3-smm3)+p4#o@aqay#buqw7v=4-0nn^!y%zmlltztdsxi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*', 'https://vercel-django-gules.vercel.app/']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "corsheaders",
+    # "corsheaders",
 ]
 
 MIDDLEWARE = [
@@ -50,12 +50,19 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "corsheaders.middleware.CorsMiddleware",
+    # "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = 'sitewsgi.urls'
 
-CORS_ALLOWED_ORIGINS = True
+# CORS_ORIGIN_ALLOW_ALL = True
+# CORS_ALLOWED_ORIGINS = True
+# CORS_ALLOWED_ORIGINS = [
+#     "https://example.com",
+#     "https://sub.example.com",
+#     "http://localhost:8080",
+#     "http://127.0.0.1:9000",
+# ]
 
 TEMPLATES = [
     {
@@ -91,7 +98,7 @@ DATABASES = {
         'default': {
             'ENGINE': 'djongo',
             'NAME': 'vercel_idriver_education_db',
-            'ENFORCE_SCHEMA': False,
+            'ENFORCE_SCHEMA': True,
             'CLIENT': {
                 'host': 'mongodb+srv://lalchanbadsa12345:CaTx1DZMsMqrovtY@cluster0.z9qtx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
             }  
